da0b9c9fc6a74190bdab3d5c75f319f78eb0448b
dbeee <dbeee.acharya@gmail.com>
2024-03-14 15:00:50 +0545

    basic login with jsp

---
+++ EcliplseWorkspace/W4C9-JSP/src/main/java/userLogin/Login.java
@@ -0,0 +1,58 @@
+package userLogin;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet(asyncSupported = true, urlPatterns = { "/Login" })
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String password = request.getParameter("password");
+		String email = request.getParameter("email");
+		
+		String correctPassword = "1234";
+		String correctEmail = "a@a.com";
+		
+		if(correctEmail.equals(email) && correctPassword.equals(password)) {
+			RequestDispatcher dispatcher = request.getRequestDispatcher("home");
+			dispatcher.forward(request, response);
+			
+		} else {
+			String login_failed_message = "username or password incorrect";
+			request.setAttribute("error_message", login_failed_message);
+			RequestDispatcher dispatcher = request.getRequestDispatcher("pages/login.jsp");
+			dispatcher.forward(request, response);
+			
+		}
+	}
+
+}
---
+++ EcliplseWorkspace/W4C9-JSP/src/main/webapp/pages/login.jsp
@@ -0,0 +1,30 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	
+	<%
+	String message = (String)request.getAttribute("error_message");
+	
+	if((message != null && !message.isEmpty())){
+		%> <p style = "color: red"> <%= message %> 
+	<%	
+	}
+	
+	%>
+
+<form action="Login" method="post">
+	<p>Email: <input type="email" name="email"></p>
+	<p>Password: <input type="password" name="password"></p>
+	<br>
+	<input type = "submit" name = "login">
+	
+</form>
+	
+</body>
+</html>
